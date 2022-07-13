f48f6b4b06a88593d3e4f67902a28bef709f5352
Almadavic <almadavic@live.com>
2022-07-13 18:02:07 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.dto.SaleDto;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,9 @@ public class SaleController {
     @Autowired
     public SaleService saleService;
 
+    @Autowired
+    public SmsService smsService;
+
     @GetMapping
     public ResponseEntity<Page<SaleDto>> findSales(@RequestParam(value="minDate", defaultValue = "") String minDate,
                                                  @RequestParam(value="maxDate", defaultValue = "") String maxDate,
@@ -24,5 +28,13 @@ public class SaleController {
         Page<SaleDto> sales = saleService.findSales(minDate,maxDate, pageable);
 
         return ResponseEntity.ok().body(sales);
+    }
+
+    @GetMapping("/{id}/notification")
+    public ResponseEntity<String> notifySms(@PathVariable Long id) {
+
+       String message =  smsService.sendSms(id);
+
+        return ResponseEntity.ok().body(message);
     }
 }
--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale,Long> {
-
     @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
     Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
